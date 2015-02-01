b804d4f62f51af2578724b9b68d23db63d1182ec
unknown <bi0ekcc@Rory.dorks.co>
2015-02-01 17:39:38 -0500

    Simulator JS and amazon tweaks

---
+++ js/simulator.js
@@ -0,0 +1,17 @@
+function changeLinkHover(string) {
+	document.getElementById("hover-link").innerHTML = string;
+}
+
+function changeAddressBar(protocol, url, trusted) {
+	var address = '<span id="';
+	if(trusted == true)
+		address += "https-verified";
+	else
+		address += "https-fail";
+		
+	address += '">' + protocol + '</span>://' + url;
+	
+	address = address.substring(0, 100)
+	
+	document.getElementById("url-bar").innerHTML = address;
+}
