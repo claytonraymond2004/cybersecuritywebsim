1705af13ade1d2b5bb66c5d35169b80d14411b4f
unknown <bi0ekcc@Rory.dorks.co>
2015-02-01 19:50:36 -0500

    Styling changes, navigation changes

--- js/simulator.js
+++ js/simulator.js
@@ -5,15 +5,23 @@ function changeLinkHover(string) {
 }
 
 function changeAddressBar(protocol, url, trusted) {
-	var address = '<span id="';
-	if(trusted == true)
-		address += "https-verified";
-	else
-		address += "https-fail";
-		
-	address += '">' + protocol + '</span>://' + url;
-	if(address.length > 100)
-	   address = address.substring(0, 100) + "...";
+    var address = '<span id="';
+    if(trusted == true)
+        address += "https-verified";
+    else
+        address += "https-fail";
+    address += '">' + protocol + '</span>://' + url;
+    
+    var trimlength = 75;
+	if(address.length > trimlength)
+	   address = address.substring(0, trimlength) + "...";
 	
 	document.getElementById("url-bar").innerHTML = address;
+}
+
+function changeAddressBarSimple(string) {
+    var trimlength = 75;
+	if(string.length > trimlength)
+	   string = string.substring(0, trimlength) + "...";
+	document.getElementById("url-bar").innerHTML = string;
 }
