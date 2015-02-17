0c387874d62edc5142ec21656f598d11b59a52c6
Clayton Colwell <claytonraymond2004@gmail.com>
2015-02-17 14:52:32 -0500

    Edit search bar with results

--- js/search.js
+++ js/search.js
@@ -4,6 +4,8 @@ function prevResult() {
     if(search > 0) {
         document.getElementById("resultList-" + search.toString()).style.display = "none";
         search--;
+        var query = document.getElementById("resultList-" + search.toString()).getAttribute("data-query");
+        setSearch(query);
         document.getElementById("resultList-" + search.toString()).style.display = "block";
     }
 }
@@ -13,6 +15,12 @@ function nextResult() {
     if(search < contentItems.length-1) {
         document.getElementById("resultList-" + search.toString()).style.display = "none";
         search++;
+        var query = document.getElementById("resultList-" + search.toString()).getAttribute("data-query");
+        setSearch(query);
         document.getElementById("resultList-" + search.toString()).style.display = "block";
     }
+}
+
+function setSearch(query) {
+    $('[id^=searchBar]').text(query);
 }
